67c8559fc290c9b52972181f04b2bbce6c764ac6
Jomagus <github@jomagus.de>
2016-01-27 16:26:58 +0100

    Erste Implementierung

---
+++ src/HauptMenueEventManagment.java
@@ -0,0 +1,22 @@
+import javafx.stage.Stage;
+
+/**
+ * Verwaltet Hauptmenueevents
+ */
+public class HauptMenueEventManagment {
+    private Stage PrimaereStage;
+
+    /**
+     * Initialisiert einen EventManager fuers Hauptmenue
+     *
+     * @param VaterStage Die Stage mit dem Hauptmenue
+     */
+    public HauptMenueEventManagment(Stage VaterStage) {
+        this.PrimaereStage = VaterStage;
+    }
+
+    public void FilterSubMenue() {
+
+    }
+
+}
